61835fd317e99f1523a66042a1ce2060b6186e14
yoris29 <ydoumi29@gmail.com>
2025-03-07 18:52:38 +0100

    Tags

--- frontend/src/components/input/TagInput.jsx
+++ frontend/src/components/input/TagInput.jsx
@@ -1,25 +1,44 @@
 import PropTypes from "prop-types";
-import { Plus } from "lucide-react";
-import { useState } from "react";
+import { Plus, X } from "lucide-react";
+import { useRef, useState } from "react";
 
 export const TagInput = ({ tags, setTags }) => {
   const [inputValue, setInputValue] = useState("");
+  const tagInput = useRef(null);
 
   const addTags = () => {
     if (inputValue.trim() !== "") {
       // Set the tags array into the old array plus the new tag
       setTags([...tags, inputValue.trim()]);
       setInputValue("");
+      tagInput.current.value = "";
     }
+  };
+
+  const removeTag = (i) => {
+    setTags(tags.filter((tag) => tag !== i));
+    console.log(i);
   };
 
   return (
     <div className="flex items-center gap-3">
+      <div className="flex gap-2">
+        {tags.map((tag, index) => (
+          <span className="flex items-center bg-blue-400" key={index}>
+            #{tag}
+            <X
+              className="cursor-pointer ml-2"
+              onClick={removeTag}
+              size={"20px"}
+            />
+          </span>
+        ))}
+      </div>
       <input
+        ref={tagInput}
         className="border border-gray-400 p-2 rounded-sm w-72"
         type="text"
         placeholder="Add Tags"
-        value={tags}
         onChange={(e) => setInputValue(e.target.value)}
       />
       <button>
