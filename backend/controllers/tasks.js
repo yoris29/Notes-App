ce276d419e957b7866c0d31656e259cf9b89cd1b
yoris29 <ydoumi29@gmail.com>
2025-04-10 17:39:04 +0100

    Added login and create note

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
+const auth = require("../auth/auth");
+const Note = require("../models/note.model");
 
 const hello = (req, res) => {
   res.json({ data: "Hello" });
@@ -39,4 +41,56 @@ const createAccount = async (req, res) => {
   });
 };
 
-module.exports = { hello, createAccount };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ error: true, msg: "Please enter valid credentials" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res
+      .status(400)
+      .json({ error: true, msg: "No account linked with this email address" });
+  }
+
+  if (user.password == password) {
+    const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    return res.json({ error: false, msg: "Login Successful", email, token });
+  } else {
+    return res.status(400).json({ error: false, msg: "Invalid password" });
+  }
+};
+
+const createNote = async (req, auth, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
+
+  if (!title || !content) {
+    return res.status(400).json({
+      error: true,
+      msg: "You have to enter both the note title and content",
+    });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    });
+    await note.save();
+    return res.json({ error: false, msg: "Note added successfuly" });
+  } catch (err) {
+    return res.status(500).json({ error: true, msg: "Internal Server Error" });
+    console.log(err);
+  }
+};
+
+module.exports = { hello, createAccount, login, createNote };
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { hello, createAccount } = require("../controllers/tasks");
+const {
+  hello,
+  createAccount,
+  login,
+  createNote,
+} = require("../controllers/tasks");
 
 router.route("/").get(hello);
+router.route("/login").post(login);
 router.route("/createAccount").post(createAccount);
+router.route("/addNote").post(createNote);
 
 module.exports = router;
