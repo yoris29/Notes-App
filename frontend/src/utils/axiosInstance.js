4785fe621bca72f6b5aa11c05d8611d064139bd8
yoris29 <ydoumi29@gmail.com>
2025-06-14 18:20:09 +0100

    Updated the axios instance

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -9,4 +9,15 @@ const axiosInstance = axios.create({
   },
 });
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    accessToken ? (config.headers.Authorization = `Bearer ${accessToken}`) : "";
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
 export default axiosInstance;
